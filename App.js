04a855a6290068f59344fad44f51a39e783cca98
tunde <ayodeleolumidetunde@gmail.com>
2022-07-26 09:27:37 -0400

    addition to entry point

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4f4f4f4',
     alignItems: 'center',
     justifyContent: 'center',
   },
